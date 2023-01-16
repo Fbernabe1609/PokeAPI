dd35efe20db42dbe2035043d05b99aa77fee8824
Fbernabe1609 <fbernabe160903@gmail.com>
2023-01-16 14:14:51 +0100

    Avance

--- app/src/main/java/com/tareaapi/pokeapi/views/PokemonActivity.java
+++ app/src/main/java/com/tareaapi/pokeapi/views/PokemonActivity.java
@@ -3,18 +3,15 @@ package com.tareaapi.pokeapi.views;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
-import com.tareaapi.pokeapi.MainActivity;
 import com.tareaapi.pokeapi.R;
 import com.tareaapi.pokeapi.models.Pokemon;
-import com.tareaapi.pokeapi.models.PokemonList;
-import com.tareaapi.pokeapi.models.Sprites;
 import com.tareaapi.pokeapi.service.PokemonLoader;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.List;
 import java.util.Objects;
 
 import retrofit2.Call;
@@ -24,6 +21,7 @@ import retrofit2.Response;
 public class PokemonActivity extends AppCompatActivity{
 
     String pokemonName = PokemonAdapter.pokemonNameData;
+    TextView types, species, baseExperience;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,13 +34,23 @@ public class PokemonActivity extends AppCompatActivity{
         Objects.requireNonNull(getSupportActionBar()).hide();
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
+        types = (TextView) findViewById(R.id.TypesTextView);
+        species = (TextView) findViewById(R.id.SpeciesTextView);
+        baseExperience = (TextView) findViewById(R.id.BaseExperienceTextView);
+
         PokemonLoader loader = new PokemonLoader();
         Call<Pokemon> call = loader.getPokemonData(pokemonName);
         call.enqueue(new Callback<Pokemon>() {
             @Override
             public void onResponse(Call<Pokemon> call, Response<Pokemon> response) {
                 Pokemon pokemon = response.body();
-                System.out.println(pokemon.getName());
+                String text = types.getText().toString();
+                for (int i = 0; i < pokemon.getTypes().size(); i++){
+                    text = text + pokemon.getTypes().get(i) + " ";
+                }
+                types.setText(text);
+                species.setText(pokemon.getSpecies().getName());
+//                baseExperience.setText(pokemon.getBaseExperience());
                 Toast.makeText(PokemonActivity.this, pokemon.getName(), Toast.LENGTH_LONG).show();
             }
 
